2cd0dbff15b016c06350f53e8086ba49f4248ed5
shayan-webdeveloper <shayanadec2@gmail.com>
2026-09-09 15:52:48 +0500

    Connecting Supabase With Library

--- Frontend/lib/supabase/server.js
+++ Frontend/lib/supabase/server.js
@@ -1,3 +1,4 @@
+// lib/supabase/server.js
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
@@ -5,14 +6,19 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
-     cookies: getAll() {
-          return cookieStore.getAll() 
-     },
-     setAll(cookiesToSet){
-          cookieStore.set(cookiesToSet)
-     }
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          )
+        },
+      },
     }
+  )
 }
