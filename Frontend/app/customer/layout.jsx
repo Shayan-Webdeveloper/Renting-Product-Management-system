f55e22012db3cceb24414a94852f4e4b5da080dd
shayan-webdeveloper <shayanadec2@gmail.com>
2026-09-11 12:12:57 +0500

    Deployment issues resolved

--- Frontend/app/customer/layout.jsx
+++ Frontend/app/customer/layout.jsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase/server'
-import CustomerSidebar from '@/app/Components/CustomerSidebar'
+import CustomerSidebar from '@/app/C  omponents/CustomerSidebar'
 
 export default async function CustomerLayout({ children }) {
   const supabase = await createClient()
